afbe5894fe1c13f5932d40a8b821e3a72a9b5680
minjekang <kangminje777@gmail.com>
2021-11-16 20:19:19 +0900

    popup

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,9 @@
 function popup() {
     var result = confirm("Are you sure you want to buy it?");
-        
-    if(result)
-    {
+
+    if (result) {
         alert("Your purchase is completed!")
-    }
-    else
-    {
+    } else {
 
     }
 }
